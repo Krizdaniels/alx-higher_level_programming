2a831e2f3f9f22640d11321f07df5881b039ad7e
Krizdaniels <dannychris2@gmail.com>
2023-11-01 14:43:22 -0700

    python Project

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author - Ogah Daneil
+
+def magic_calculation(a, b, c):
+    ***Match bytecode provide by ALx School. ***
+    if a < b:
+        return(c)
+    if c > b
+    return (a + b)
+return (a*b - c)
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,5 @@
-#!/usr/bin/python3
-def to_uper(character):
-    if ord(character) >= 97 and ord(character) <= 122:
-        return (ord(character) - 32)
-    else:
-        return ord(character)
+#!/usr/bin/env python3
+uppercase = __import__('8-uppercare').uppercase
 
-
-def uppercase(str):
-    new = ""
-    for character in str:
-        new += "%c" % to_uper(character)
-        print("{:s}".format(new))
+uppercase("best")
+uppercase("Best School 98 Battery street")
