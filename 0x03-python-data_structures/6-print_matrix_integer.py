b04fe695de06864761dc42607bae8ac5e026e024
Krizdaniels <dannychris2@gmail.com>
2023-11-06 15:33:40 -0800

    0x03 py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matri=[[]]):
+    for n in range(len(matrix)):
+        for j in range(len(matric[n])):
+            print("{:d}".format(matrix[n][j], end="")
+                    if j != (len(matrix[n]) - 1):
+                    print(" ", end="")
+
+
+                    print("")
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list)
+if len(my_list) == 0:
+    return (None)
+
+max_num - my_list[0]
+for n in range(len(my_list))
+max_num = mya-list[n]
+
+returna (max_num)
