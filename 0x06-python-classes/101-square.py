4f22596192eef180fc0fe60c6fcc0796aa927e44
Krizdaniels <dannychris2@gmail.com>
2023-11-21 13:10:57 -0800

    0x06 python

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -3,14 +3,13 @@
 
 
 class Square:
-    """
-    The class square that has attributes:
-        size
-    some attributes are protected from input.
-    """
+    """Represent a square."""
+
     def __init__(self, size=0, position=(0, 0)):
-        """
-        The initialization function for our square clasee
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
         """
         if self.__validate_size(size):
             self.__size = size
