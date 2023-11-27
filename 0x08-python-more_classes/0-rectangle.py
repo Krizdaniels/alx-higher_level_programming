6a02cc4efe335093ffd8e01c07eabb78ee304653
Krizdaniels <dannychris2@gmail.com>
2023-11-27 13:58:40 -0800

    0x08 Python Project

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""0-rectangle, built for ALX SE project 0x08 Python task 0.
+"""
+
+
+class Rectangle:
+    """The empty class per task instructions, will be built upon in later tasks.
+    """
+    pass
