252f0b60750d6bfc8b563f62c1e35934129380b8
Krizdaniels <dannychris2@gmail.com>
2024-02-14 14:00:04 -0800

    Javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 // To prints 3 lines: (like 1-multi_languages.js) but by
 // using an array of string and a loop
-
 const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < 3; i++) {
