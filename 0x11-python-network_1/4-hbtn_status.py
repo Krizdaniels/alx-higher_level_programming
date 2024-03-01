37462af1b0756fd3bc1bd4515e13d4f7c9934814
Krizdaniels <dannychris2@gmail.com>
2024-03-01 15:26:32 -0800

    Status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,10 +2,16 @@
 """It fetches https://intranet.hbtn.io/status using requests"""
 import requests
 
-
 if __name__ == "__main__":
     """fetches https://intranet.hbtn.io/status using requests"""
-    r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    try:
+        response = requests.get('https://intranet.hbtn.io/status')
+        response.raise_for_status()  # Check if the request was successful
+
+        print("Body response:")
+        print(f"\t- type: {type(response.text)}")
+        print(f"\t- content: {response.text}")
+
+    except requests.RequestException as e:
+        print(f"Error making the request: {e}")
+
