30f6ef5d745469ee9b63ddeb9c40b75717acb99d
Krizdaniels <dannychris2@gmail.com>
2024-03-01 15:33:41 -0800

    Status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""A script for testing status of web pages
-"""
+#a Python script that fetches https://alx-intranet.hbtn.io/status
+from urllib import request
+
 if __name__ == "__main__":
-    import urllib.request
-    url = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        bytes_content = response.read()
-        content = bytes_content.decode('utf-8')
-        print_str = '''Body response:
-\t- type: {}
-\t- content: {}
-\t- utf8 content: {}'''.format(type(bytes_content), bytes_content, content)
-        print(print_str)
+    url = 'https://alx-intranet.hbtn.io/status'
+    
+    with request.urlopen(url) as response:
+        content = response.read()
+
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
