ec02dd4fd21b3d49d646f2be0b2960e5aee4a641
Krizdaniels <dannychris2@gmail.com>
2024-03-01 15:17:20 -0800

    Commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
 """
-The list 10 commits (from the most recent to oldest) the repository “rails”
-by the user “rails”
+List 10 commits (from the most recent to oldest) of the repository
+“rails” by the user “rails”
 """
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
-    """
-    list 10 commits (from the most recent to oldest) of the repository
-    “rails” by the user “rails”
-    """
+    if len(argv) != 3:
+        print("Usage: {} <repository> <owner>".format(argv[0]))
+        exit(1)
+
     repo = argv[1]
     owner = argv[2]
+    
     url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
-    r = requests.get(url)
-    res_list = r.json()
     try:
-        for i in range(10):
-            print("{}: {}".format(res_list[i].get('sha'), res_list[i].
-                                  get('commit').get('author').get('name')))
-    try:
-        pass
+        r = requests.get(url)
+        r.raise_for_status()  # Check for request errors
+        res_list = r.json()
+
+        for i in range(min(10, len(res_list))):
+            print("{}: {}".format(res_list[i]['sha'], res_list[i]['commit']['author']['name']))
+
+    except requests.RequestException as e:
+        print("Error making the request:", e)
+    except IndexError:
+        print("Not enough commits available.")
+
