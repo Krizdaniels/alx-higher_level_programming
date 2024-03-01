78358cf9ed40a3c27e6985a2d21621d095920479
Krizdaniels <dannychris2@gmail.com>
2024-03-01 15:35:35 -0800

    Status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-#a Python script that fetches https://alx-intranet.hbtn.io/status
+# a Python script that fetches https://alx-intranet.hbtn.io/status
 from urllib import request
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
-    
+
     with request.urlopen(url) as response:
         content = response.read()
 
